4b8fc01ce8060cdeca28f766418010376a78cec3
Sathish <mail2sathish11@gmail.com>
2022-06-18 10:12:18 +0530

    git add .!

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Machine Learning Project"
+    return "Machine Learning Project!"
 
 
 if __name__ == '__main__':
