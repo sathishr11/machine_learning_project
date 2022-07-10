fb0bd8b8483d10cfb14a04d35e08c2b6ce288859
Sathish <mail2sathish11@gmail.com>
2022-07-10 13:27:58 +0530

    Updated pipeline for model training

--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -2,11 +2,12 @@ from housing.config.configuration import Configuartion
 from housing.logger import logging
 from housing.exception import HousingException
 
-from housing.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact
+from housing.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact, ModelTrainerArtifact
 from housing.entity.config_entity import DataIngestionConfig
 from housing.component.data_ingestion import DataIngestion
 from housing.component.data_validation import DataValidation
 from housing.component.data_transformation import DataTransformation
+from housing.component.model_trainer import ModelTrainer
 import os,sys
 
 class Pipeline:
@@ -50,8 +51,14 @@ class Pipeline:
         except Exception as e:
             raise HousingException(e,sys)
 
-    def start_model_trainer(self):
-        pass
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(model_trainer_config=self.config.get_model_trainer_config(),
+                                         data_transformation_artifact=data_transformation_artifact
+                                         )
+            return model_trainer.initiate_model_trainer()
+        except Exception as e:
+            raise HousingException(e, sys) from e
 
     def start_model_evaluation(self):
         pass
@@ -69,6 +76,7 @@ class Pipeline:
                 data_ingestion_artifact=data_ingestion_artifact,
                 data_validation_artifact=data_validation_artifact
             )
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
 
             
 
